574ff294e9ead84bb953170cabdcdff52789916f
DarkWister <darkwistercrak17@gmail.com>
2025-03-28 17:26:53 +0000

    removed folder, added a user deppendant SideMenu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,5 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'folder/inbox',
     pathMatch: 'full',
-  },
-  {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
-  },
+  }
 ];
