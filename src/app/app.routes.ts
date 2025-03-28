e2d73b765efbe20f6e4b0287d75ac7d4fb5d3467
DarkWister <darkwistercrak17@gmail.com>
2025-03-28 18:04:39 +0000

    approuter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'folder/inbox',
     pathMatch: 'full',
+  },
+  {
+    path: 'inicio-user',
+    loadComponent: () => import('./pages/inicio-user/inicio-user.page').then( m => m.InicioUserPage)
   }
 ];
