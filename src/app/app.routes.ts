d9d8da17df8c9f0d79f8a1a1b4fb34d1fe7b2a80
Shunazo <abeljunior0907@gmail.com>
2025-03-28 14:09:04 -0400

    login added, route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'folder/inbox',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   }
+
 ];
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonFooter, IonItem, IonList, IonIcon } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonInput, IonFooter, IonItem, IonList, IonIcon]
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
