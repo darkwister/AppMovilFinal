022e09247e991fc0301668381df60ee5a9d6fce9
Shunazo <abeljunior0907@gmail.com>
2025-03-28 15:51:39 -0400

    noticia-detalle added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,10 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },  {
+    path: 'noticia-detalle',
+    loadComponent: () => import('./pages/noticia-detalle/noticia-detalle.page').then( m => m.NoticiaDetallePage)
   }
+
 
 ];
