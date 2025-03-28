77751203ab47bf187f7ced738831ad7049131f9f
DarkWister <darkwistercrak17@gmail.com>
2025-03-28 19:01:18 +0000

    InicioUser done

--- src/app/pages/inicio-user/inicio-user.page.ts
+++ src/app/pages/inicio-user/inicio-user.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonIcon, IonNavLink, IonProgressBar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonIcon, IonNavLink, IonProgressBar, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-inicio-user',
   templateUrl: './inicio-user.page.html',
   styleUrls: ['./inicio-user.page.scss'],
   standalone: true,
-  imports: [IonProgressBar, IonNavLink, IonIcon, IonButton, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCol, IonRow, IonGrid, IonProgressBar, IonNavLink, IonIcon, IonButton, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class InicioUserPage implements OnInit {
   username = 'Usuario hipotetico';
