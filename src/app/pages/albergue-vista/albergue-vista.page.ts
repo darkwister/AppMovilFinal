8508f2a81426cde084fad8efdaf57e311ee596a2
DarkWister <darkwistercrak17@gmail.com>
2025-03-28 22:36:02 +0000

    testing albergue vista

--- src/app/pages/albergue-vista/albergue-vista.page.ts
+++ src/app/pages/albergue-vista/albergue-vista.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonProgressBar, IonList, IonItemSliding, IonItem, IonLabel, IonItemOptions, IonItemOption } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-albergue-vista',
   templateUrl: './albergue-vista.page.html',
   styleUrls: ['./albergue-vista.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonItemOption, IonItemOptions, IonLabel, IonItem, IonItemSliding, IonList, IonProgressBar, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AlbergueVistaPage implements OnInit {
-
+  isLoading: boolean = false;
+  Albergues: any[] = [
+    {
+      ciudad:	"Distrito Nacional",
+      codigo:	"DO-010002",
+      edificio:	"Polideportivo San Carlos",
+      coordinador:	"Luis Peña",
+      telefono:	"(809) 308-3411",
+      capacidad:	"274 personas",
+      lat:	"-69.89178",
+      lng:	"18.47893"
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  goDetails(){
+    console.log('Detalles albergue');
+  }
 }
