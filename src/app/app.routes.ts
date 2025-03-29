5b6bf78eac9bb1ffb7d8b0001d4ae1563431f0d6
DarkWister <101299728+darkwister@users.noreply.github.com>
2025-03-29 18:15:28 +0000

    inicio nonuser half implemented. TODO the sign in button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/example',
+    redirectTo: '/inicio-nonuser',
     pathMatch: 'full',
   },
   {
--- src/app/pages/inicio-nonuser/inicio-nonuser.page.ts
+++ src/app/pages/inicio-nonuser/inicio-nonuser.page.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonAvatar, IonText, IonLabel, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-inicio-nonuser',
   templateUrl: './inicio-nonuser.page.html',
   styleUrls: ['./inicio-nonuser.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCol, IonRow, IonGrid, IonLabel, IonText, IonAvatar, IonImg, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class InicioNonuserPage implements OnInit {
-
+  images = [
+    'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    'https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    'https://ionicframework.com/docs/img/demos/thumbnail.svg'
+  ]
   constructor() { }
 
   ngOnInit() {
