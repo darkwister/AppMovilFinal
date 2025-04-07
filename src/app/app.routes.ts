82e0f00934bfa54e9b1ce965d18cb144dac3eed8
DarkWister <101299728+darkwister@users.noreply.github.com>
2025-04-07 20:24:29 +0000

    missing routes fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,5 +21,13 @@ export const routes: Routes = [
   {
     path: 'noticia-detalle',
     loadComponent: () => import('./pages/noticia-detalle/noticia-detalle.page').then( m => m.NoticiaDetallePage)
+  },
+  {
+    path: 'albergue-vista',
+    loadComponent: () => import('./pages/albergue-vista/albergue-vista.page').then( m => m.AlbergueVistaPage)
+  },
+  {
+    path: 'inicio-nonuser',
+    loadComponent: () => import('./pages/inicio-nonuser/inicio-nonuser.page').then( m => m.InicioNonuserPage)
   }
 ];
